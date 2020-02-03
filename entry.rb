7ad3d2223db244fb7b20f08017b72b922ba34030
Lulu Zheng <luluzheng1@gmail.com>
2020-02-03 14:28:15 -0500

    comment in entry

--- entry.rb
+++ entry.rb
@@ -1,5 +1,5 @@
 class Entry
-    # take in an entry hash object and initialize an Entry object
+    # Take in an entry hash object and initialize an Entry object
     def initialize(e)
         @EntryType = e["EntryType"]
         @EntryName = e["EntryName"]
@@ -8,7 +8,7 @@ class Entry
         @SelectionWeight = e["SelectionWeight"]
     end
 
-    # return a random number between MinDrops and Max Drops
+    # Teturn a random number between MinDrops and Max Drops
     def select_amount
         amount = rand(min..max)
         return amount
